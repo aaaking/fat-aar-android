9d57c4f11e583ea006bb6b31eea487970f67a396
zhouzhihui <zhouzhihui@kuaishou.com>
2024-01-06 03:32:54 +0800

    fa

--- project/config/src/main/java/com/zzh/yummy/config/ConfigPlugin.java
+++ project/config/src/main/java/com/zzh/yummy/config/ConfigPlugin.java
@@ -2,6 +2,10 @@ package com.zzh.yummy.config;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.artifacts.Configuration;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * author: zhouzhihui
@@ -9,7 +13,19 @@ import org.gradle.api.Project;
  * description:
  */
 public class ConfigPlugin implements Plugin<Project> {
+    public static final String ARTIFACT_TYPE_AAR = "aar";
 
+    public static final String ARTIFACT_TYPE_JAR = "jar";
+
+    private static final String CONFIG_NAME = "embed";
+
+    public static final String CONFIG_SUFFIX = "Embed";
+
+    private Project project;
+
+    private RClassesTransform transform;
+
+    private final Collection<Configuration> embedConfigurations = new ArrayList<>();
     @Override
     public void apply(Project project) {
     }
