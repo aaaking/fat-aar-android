7dda3f937e1615b2f6457fe2c6d8a0d53e0e0014
zhouzhihui <zhouzhihui@kuaishou.com>
2024-01-06 02:35:55 +0800

    fa

---
+++ project/config/src/main/java/BuildConfig.kt
@@ -0,0 +1,12 @@
+/**
+ * author: zhouzhihui
+ * created on: 2024/1/6 01:59
+ * description:
+ */
+object BuildConfig {
+    const val compileSdkVersion = 34
+    const val minSdkVersion = 18
+    const val targetSdkVersion = 33
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
---
+++ project/config/src/main/java/com/zzh/yummy/config/ConfigPlugin.java
@@ -0,0 +1,16 @@
+package com.zzh.yummy.config;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+/**
+ * author: zhouzhihui
+ * created on: 2024/1/6 01:40
+ * description:
+ */
+public class ConfigPlugin implements Plugin<Project> {
+
+    @Override
+    public void apply(Project project) {
+    }
+}
