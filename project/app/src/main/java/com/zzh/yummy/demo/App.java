cebb198441c5e253cb2721e0e2548bf7f872f406
zhouzhihui <zhouzhihui@kuaishou.com>
2024-01-06 02:53:51 +0800

    fa

---
+++ project/app/src/main/java/com/zzh/yummy/demo/App.java
@@ -0,0 +1,15 @@
+package com.zzh.yummy.demo;
+
+import android.app.Application;
+
+/**
+ * author: zhouzhihui
+ * created on: 2024/1/6 02:53
+ * description:
+ */
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
