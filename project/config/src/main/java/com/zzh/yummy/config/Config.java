4c0d21362e68a6b9737e8741981805421dad2e95
zhouzhihui <zhouzhihui@kuaishou.com>
2024-01-06 02:42:11 +0800

    fa

---
+++ project/config/src/main/java/com/zzh/yummy/config/Config.java
@@ -0,0 +1,12 @@
+package com.zzh.yummy.config;
+
+/**
+ * author: zhouzhihui
+ * created on: 2024/1/6 02:36
+ * description:
+ */
+public class Config {
+    public static int compileSdkVersion = 34;
+    public static int minSdkVersion = 18;
+    public static int targetSdkVersion = 33;
+}
