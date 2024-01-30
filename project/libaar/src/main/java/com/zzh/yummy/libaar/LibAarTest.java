3c5269ed8b757a36301634bfaca749a56428da2b
zhouzhihui <zhouzhihui@kuaishou.com>
2024-01-30 19:25:04 +0800

    fa

---
+++ project/libaar/src/main/java/com/zzh/yummy/libaar/LibAarTest.java
@@ -0,0 +1,14 @@
+package com.zzh.yummy.libaar;
+
+import com.kwai.video.ksmediaplayerkit.KSMediaPlayerBuilder;
+
+/**
+ * author: zhouzhihui
+ * created on: 2024/1/30 19:19
+ * description:
+ */
+public class LibAarTest {
+    void test() {
+        KSMediaPlayerBuilder builder = new KSMediaPlayerBuilder(null);
+    }
+}
