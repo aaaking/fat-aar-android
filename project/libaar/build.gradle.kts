93d93870f214c679b57e9fabbf0b1ee403c88a0c
zhouzhihui <zhouzhihui@kuaishou.com>
2024-01-30 19:27:53 +0800

    fa

--- project/libaar/build.gradle.kts
+++ project/libaar/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         minSdk = 24
-        // testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
 
@@ -33,9 +33,9 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
     implementation(files("$projectDir/../aar/streamlake-mediaplayer-1.8.3.7.aar"))
-    // testImplementation("junit:junit:4.14-SNAPSHOT")
-    // androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    // androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    testImplementation("junit:junit:4.14-SNAPSHOT")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- project/libaar/src/androidTest/java/com/zzh/yummy/libaar/ExampleInstrumentedTest.kt
+++ project/libaar/src/androidTest/java/com/zzh/yummy/libaar/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.zzh.yummy.libjava
+package com.zzh.yummy.libaar
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
